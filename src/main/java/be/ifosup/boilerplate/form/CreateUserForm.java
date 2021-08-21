b848c0250cff00edf61427ab33ff86d4c357356d
Sébastien Lejeune <seb.lejeune.web@gmail.com>
2021-08-21 23:55:09 +0200

    Prepare user validation

--- src/main/java/be/ifosup/boilerplate/form/CreateUserForm.java
+++ src/main/java/be/ifosup/boilerplate/form/CreateUserForm.java
@@ -2,11 +2,26 @@ package be.ifosup.boilerplate.form;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class CreateUserForm {
+    @NotNull(message = "First name obligatoire")
     private String firstname;
+
+    @NotNull(message = "Last name obligatoire")
     private String lastname;
+
+    @NotNull(message = "Email obligatoire")
+    @Email
     private String emailaddress;
+
+    @NotNull(message = "Username obligatoire")
     private String username;
+
+    @NotNull(message = "Password obligatoire")
+    @Size(min = 10, message = "Min 10 caratères")
     private String password;
 }
--- src/main/java/be/ifosup/boilerplate/web/admin/ManageUsersController.java
+++ src/main/java/be/ifosup/boilerplate/web/admin/ManageUsersController.java
@@ -1,11 +1,14 @@
 package be.ifosup.boilerplate.web.admin;
 
+import be.ifosup.boilerplate.dto.UserDTO;
 import be.ifosup.boilerplate.form.CreateUserForm;
 import be.ifosup.boilerplate.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -29,12 +32,16 @@ public class ManageUsersController {
     }
 
     @GetMapping("/create-user-page")
-    public String createUserPage() {
+    public String createUserPage(CreateUserForm user, Model model) {
+        model.addAttribute("user", new CreateUserForm());
         return "admin/user/create";
     }
 
     @PostMapping("/create-user")
-    public String validateCreation(@Valid CreateUserForm createUserForm) {
+    public String validateCreation(@Valid @ModelAttribute("user") CreateUserForm createUserForm, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) {
+            return "admin/user/create";
+        }
         userService.create(createUserForm);
         return "admin/manage-users";
     }
